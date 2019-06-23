20258a96dac7a63dbbfc4ad01af7bedda5e58cbd
Adam ? <adamznow@gmail.com>
2019-06-23 01:13:09 -0700

    Initial Commit

---
+++ Assets/Scenes/TestMonoBehaviourGenerics.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMonoBehaviourGenerics : MonoBehaviour
+{
+    public void Start()
+    {
+        CallGenericOne();
+        CallGenericTwo();
+    }
+
+    public void CallGenericOne()
+    {
+        // Compiles, should work
+        var o = Resources.FindObjectsOfTypeAll<GameObject>();
+    }
+
+    public void CallGenericTwo()
+    {
+        // Compiles, should not work (error)
+        var o = Resources.Load<GameObject>("testNonExistent");
+    }
+}
